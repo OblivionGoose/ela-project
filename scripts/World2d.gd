d0a8de4e4f2f1d1c1855533ecf66b1cec9eb5806
OblivionGoose <scottlsammy@gmail.com>
2026-02-26 10:26:16 -0500

    Time added
    Created a clock and day and night cycle to the game. The world_2d script now has the self explanatory functions clock(), pause_clock(), unpause_clock(), and Now the variables of minute, hour, day, and time of day (day 1/night 1) are accessible in the global script. If you want to change the speed of which time moves in game, just go to the timer node and change its time variable.

--- scripts/World2d.gd
+++ scripts/World2d.gd
@@ -1,8 +1,83 @@
 extends Node2D
 
+var minute: int = -5
+var hour: int = 15
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_scene_change()
+	var clock = $Timer
+	Clock()
+	clock.timeout.connect(Clock)
+	var text_edit = $RichTextLabel3
+	text_edit.add_text(str(globals.time_of_day))
+
+# Calls the timer node to simulate a military analog clock.
+# change timer wait to change the speed of time, wait time = 5 minutes in game
+func Clock():
+	var clock = $Timer
+	clock.start()
+	var clock_text = $RichTextLabel2
+	var clockMinute = "00"
+	var clockHour = "00"
+	minute = minute + 5
+	if minute == 60:
+		minute = 0
+		hour += + 1
+	if hour == 24:
+		hour = 0
+		day_change()
+	if hour < 10:
+		clockHour = "0" + str(hour)
+	else:
+		clockHour = str(hour)
+	if minute < 10:
+		clockMinute = "0" + str(minute)
+	else:
+		clockMinute = str(minute)
+	if hour == 6 && minute == 0:
+		night_change()
+	if hour == 21 && minute == 0:
+		night_change()
+	clock_text.clear()
+	clock_text.add_text(clockHour + ":" + clockMinute)
+
+# called when time is 00:00, i.e the day changes calendar-wise
+func day_change():
+	globals.day += 1
+
+# Called whenever daybreaks or nightfalls
+func night_change():
+	if globals.is_night: #invert current bool value of is_night 
+		globals.is_night = false
+		globals.time_of_day = "Day " + str(globals.day)
+	else:
+		globals.is_night = true
+		globals.time_of_day = "Night " + str(globals.day)
+	# process day to night transition
+	if globals.is_night:
+		pause_clock()
+	# process night to day transition
+	else:
+		pause_clock()
+	var text_edit = $RichTextLabel3
+	text_edit.clear()
+	text_edit.add_text(str(globals.time_of_day))
+
+# called whenever the clock needs to pause. call unpause_clock later
+func pause_clock():
+	var clock = $Timer
+	clock.set_paused(true)
+	var dayButton = Button.new() 
+	add_child(dayButton)
+	dayButton.scale = Vector2(10, 10)
+	dayButton.pressed.connect(unpause_clock) # unpause and remove button on pressed
+	dayButton.pressed.connect(dayButton.queue_free)
+
+func unpause_clock():
+	var clock = $Timer
+	clock.set_paused(false)
+
 
 # Called when script detects a scene change
 func _scene_change():
--- scripts/singletons/globals.gd
+++ scripts/singletons/globals.gd
@@ -4,3 +4,6 @@ var game_controller : GameController # to connect w/ game controller
 
 var scene_room: String = "Foyer" # set initial room
 var new_room: String = "Foyer"
+var day: int = 1 # set first day
+var is_night: bool = false # set day time
+var time_of_day: String = "Day 1"
